2b363cd608b95fa99111c920ca35c90133705e6f
Taylor Welling <tw3080@gmail.com>
2016-10-16 13:13:12 -0700

    trying to serve data from the Pokemon TCG API to the front-end

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
+const pokemon = require('pokemontcgsdk');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
+// app.use(express.static('build'));
+
+app.get('/', function(req, res) {
+    pokemon.card.all({ supertype: 'pokemon', name: 'charizard|blastoise|venusaur' })
+    .on('data', function (card) {
+        console.log(card);
+        res.json(card);
+    });
+});
 
 app.listen(process.env.PORT || 8080);
 
