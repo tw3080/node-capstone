3f9bb1d2f4c0604eb03cb79a36de9dac24e395a9
Taylor Welling <tw3080@gmail.com>
2016-10-25 17:37:56 -0700

    updated getCardsByName() to accept a name as a parameter and make an HTTP request to the Pokemon TCG API; added getCardById() function which accepts a card's id as a parameter and makes a request to the Pokemon TCG API to get a specific card based on that id; added userSignup() function which makes an HTTP request to the '/users' endpoint and creates a new user; added userLogin() function which makes an HTTP request to the '/login' endpoint and logs a user in if their credentials are valid

--- public/collection-app-service.js
+++ public/collection-app-service.js
@@ -2,16 +2,70 @@ import angular from 'angular';
 
 export default class CollectionAppService {
     constructor($http) {
-        this.http = $http;
+        this.$http = $http;
     }
 
-    getCardByName() {
-        console.log('get card by name');
-        let http = this.http;
-        return http({
+    /* Gets a list of cards by name of Pokemon */
+    getCardsByName(name) {
+        this.params = {
+            name: name
+        };
+
+        var controller = this;
+
+        return this.$http({
             method: 'GET',
-            url: '/cards',
-            cache: true,
+            url: 'https://api.pokemontcg.io/v1/cards',
+            params: this.params
+        }).then(function(response) {
+            controller.searchResults = response.data.cards;
+            return response.data.cards;
+        });
+    }
+
+    /* Gets a card by its unique id and returns a single card */
+    getCardById(id) {
+        this.id = id;
+
+        return this.$http({
+            method: 'GET',
+            url: 'https://api.pokemontcg.io/v1/cards/' + id,
+            cache: true
+        }).then(function(response) {
+            return response.data.card;
+        });
+    }
+
+    /* Creates a new account */
+    userSignup(email, username, password) {
+        return this.$http({
+            method: 'POST',
+            url: '/users',
+            data: {
+                email: email,
+                username: username,
+                password: password
+            }
+        });
+    }
+
+    /* Logs the user in */
+    userLogin(username, password) {
+        return this.$http({
+            method: 'POST',
+            url: '/login',
+            data: {
+                username: username,
+                password: password
+            }
+        });
+    }
+
+    /* Testing */
+    getCollection() {
+        return this.$http({
+            method: 'GET',
+            url: '/test'
         });
     }
 }
