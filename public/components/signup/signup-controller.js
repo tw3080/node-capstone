9ecdb2a755d568562b6315e4f3dd9d3034f1f243
Taylor Welling <tw3080@gmail.com>
2016-10-25 20:02:28 -0700

    added basic user creation functionality; adjusted variable names for bindings

--- public/components/signup/signup-controller.js
+++ public/components/signup/signup-controller.js
@@ -7,7 +7,18 @@ export function signupRoute($routeProvider) {
 }
 
 export default class SignupCtrl {
-    constructor() {
-        this.stuff = 'sign up page';
+    constructor(CollectionAppService) {
+        this.CollectionAppService = CollectionAppService;
+        this.email = ''; // Bound to the email input field in signup view
+        this.username = ''; // Bound to the username input field in signup view
+        this.password = ''; // Bound to the password input field in signup view
+        this.confirmPassword = ''; // Bound to the confirmPassword input field in signup view
+    }
+
+    /* Creates a new user by calling the userSignup() function from the CollectionAppService service */
+    signUp() {
+        this.CollectionAppService.userSignup(this.email, this.username, this.password).then(data => {
+            console.log(data);
+        });
     }
 }
