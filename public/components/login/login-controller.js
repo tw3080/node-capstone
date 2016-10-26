4b19beb8bad550662d6da6018c6654d16f8d0471
Taylor Welling <tw3080@gmail.com>
2016-10-25 17:42:23 -0700

    added the login() function which calls the userLogin() function from the CollectionAppService service to log the user in, and then redirects that user to their collection page

--- public/components/login/login-controller.js
+++ public/components/login/login-controller.js
@@ -7,7 +7,19 @@ export function loginRoute($routeProvider) {
 }
 
 export default class LoginCtrl {
-    constructor() {
-        this.stuff = 'login page';
+    constructor(CollectionAppService, $location) {
+        this.CollectionAppService = CollectionAppService;
+        this.location = $location; // Setting '$location' to a variable in order to use it outside the constructor function
+        this.username = ''; // Bound to username input field in login view
+        this.password = ''; // Bound to password input field in login view
+    }
+
+    /* Logs the user into their account */
+    login() {
+        var $location = this.location; // Setting 'this.location' to a variable in order to use it to redirect the user within the scope of this login() function
+
+        this.CollectionAppService.userLogin(this.username, this.password).then(data => {
+            $location.path('/collection'); // On successful login, redirect the user to their collection page
+        });
     }
 }
